d3a9796cc0e2e593f714bad8589660f66a97b3d0
william novaes <williamf.novaes@gmail.com>
2020-01-16 23:58:43 -0300

    melhoria particles starts, melhorias moon, adicionado intro scene

---
+++ Assets/Scripts/IntroTransition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IntroTransition : MonoBehaviour
+{
+    private Animator introAnim;
+    void Start()
+    {
+       StartCoroutine(ChangeScene());
+    }
+
+    IEnumerator ChangeScene() {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("StartMenu");
+    }
+}
