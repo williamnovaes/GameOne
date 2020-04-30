79484810e2768431bda3f896c2ab3bc4ec3c0150
williamnovaes <williamf.novaes@gmail.com>
2020-04-30 09:57:22 -0300

    testes network + cenarios comprados itch i

---
+++ Assets/Multiplayer/Scripts/MoveShip.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class MoveShip : NetworkBehaviour {
+    
+    [SerializeField] private float speed;
+    private Rigidbody2D playerRb;
+
+    private void Start() {
+        playerRb = GetComponent<Rigidbody2D> ();
+    }
+    
+    private void FixedUpdate() {
+        if (this.isLocalPlayer) {
+            float movement = Input.GetAxis("Horizontal");
+            playerRb.velocity= new Vector2(movement * speed, 0f);
+        }
+    }
+}
